c602f72cf41e7e639f6f6eddc70248f985787edc
Niklas Johansson <niklasjohansson@live.no>
2026-05-03 18:27:54 +0200

    Use DashboardShell layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import NewsModule from "./NewsModule";
-import SearchModule from "./SearchModule";
+import DashboardShell from "./DashboardShell";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <SearchModule />
-      <div className="max-w-5xl mx-auto px-6 py-8">
-        <NewsModule />
-      </div>
-    </div>
-  );
+  return <DashboardShell />;
 }
